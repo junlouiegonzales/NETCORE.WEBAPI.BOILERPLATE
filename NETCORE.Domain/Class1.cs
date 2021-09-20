6819d1f49620043cf598563586017cf68809ec7d
Jhun Louie Gonzales <jhun@bizbox.ph>
2021-09-20 19:00:16 +0800

    Boilerplate

---
+++ NETCORE.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NETCORE.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ NETCORE.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NETCORE.Persistence
+{
+    public class Class1
+    {
+    }
+}
